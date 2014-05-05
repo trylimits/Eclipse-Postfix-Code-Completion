32c33976302c5dd1ca376ef7aecceea6567a24f5
Nicolaj Hoess <nicohoess@gmail.com>
2014-05-05 22:40:18 +0200

    fixed signature resolving

--- org.eclipse.jdt.postfixcompletion/src/org/eclipse/jdt/postfixcompletion/core/JavaStatementPostfixContext.java
+++ org.eclipse.jdt.postfixcompletion/src/org/eclipse/jdt/postfixcompletion/core/JavaStatementPostfixContext.java
@@ -1,18 +1,25 @@
 package org.eclipse.jdt.postfixcompletion.core;
 
-import java.util.Arrays;
-
-import javax.swing.plaf.synth.SynthScrollBarUI;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.eclipse.jdt.core.ICompilationUnit;
-import org.eclipse.jdt.core.Signature;
 import org.eclipse.jdt.internal.compiler.ast.ASTNode;
 import org.eclipse.jdt.internal.compiler.ast.FieldReference;
 import org.eclipse.jdt.internal.compiler.ast.MessageSend;
 import org.eclipse.jdt.internal.compiler.ast.NameReference;
+import org.eclipse.jdt.internal.compiler.lookup.ArrayBinding;
+import org.eclipse.jdt.internal.compiler.lookup.BaseTypeBinding;
+import org.eclipse.jdt.internal.compiler.lookup.Binding;
+import org.eclipse.jdt.internal.compiler.lookup.ParameterizedTypeBinding;
+import org.eclipse.jdt.internal.compiler.lookup.ReferenceBinding;
+import org.eclipse.jdt.internal.compiler.lookup.TypeBinding;
 import org.eclipse.jdt.internal.compiler.lookup.VariableBinding;
 import org.eclipse.jdt.internal.corext.template.java.JavaContext;
-import org.eclipse.jdt.internal.corext.template.java.SignatureUtil;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.Position;
@@ -59,8 +66,6 @@ public class JavaStatementPostfixContext extends JavaContext {
 	@Override
 	public boolean canEvaluate(Template template) {
 		
-//		System.out.println("JavaStatementPostfixContext.canEvaluate()");
-
 		if (!template.getContextTypeId().equals(
 				JavaStatementPostfixContext.CONTEXT_TYPE_ID))
 			return false;
@@ -71,22 +76,37 @@ public class JavaStatementPostfixContext extends JavaContext {
 		if (currentNode == null) // We can evaluate to true only if we have a valid inner expression
 			return false;
 		
-		// if the typed in prefix does not match the template name we immediately return false
-//		System.out.println("Current prefix: " + getPrefixKey());
 		if ( template.getName().toLowerCase()
 				.startsWith(getPrefixKey().toLowerCase()) == false) {
-//			System.out.println("Not triggered bcause prefix mismatch");
 			return false;
 		}
 		
 		// We check if the template make "sense" by checking the requirements for the template
-
+		// For this purpose we have to resolve the inner_expression variable of the template
+		String regex = ("\\$\\{([a-zA-Z]+):inner_expression\\(([^\\$|\\{|\\}]*)\\)\\}");
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(template.getPattern());
+		boolean result = (matcher.groupCount() > 0) ? false : true;
 		
-		return true;
+		while (matcher.find()) {
+			String[] types = matcher.group(2).split(",");
+			for (String s : types) {
+				if (this.isNodeResolvingTo(currentNode, s.trim()) == true) {
+					return true;
+				}
+			}
+	    }
+		
+		return result;
 	}
+	
+	
+//	
+//	private boolean stringToBoolean(String input) {
+//		return "true".equalsIgnoreCase(input);
+//	}
 
 	protected String getPrefixKey() {
-//		System.out.println("JavaStatementPostfixContext.getPrefixKey()");
 		IDocument document = getDocument();
 		int start = getCompletionOffset();
 		int end = getEnd();
@@ -95,7 +115,6 @@ public class JavaStatementPostfixContext extends JavaContext {
 			while (!".".equals(temp)) {
 				temp = document.get(--start, 1);
 			}
-//			System.out.println(document.get(start + 1, end - start - 1));
 			return document.get(start + 1, end - start - 1);
 		} catch (BadLocationException e) {
 			return "";
@@ -105,17 +124,6 @@ public class JavaStatementPostfixContext extends JavaContext {
 	@Override
 	public int getEnd() {
 		return getCompletionOffset();
-	}
-	
-	@Override
-	public int getStart() {
-		
-//		System.out.println("JavaStatementPostfixContext.getStart()");
-		// TODO this is not 100% correct atm
-//		if (currentNode != null) {
-//			return getNodeBegin(currentNode);
-//		}
-		return super.getStart();
 	}
 
 	public String getOuterExpression() {
@@ -149,31 +157,72 @@ public class JavaStatementPostfixContext extends JavaContext {
 	}
 	
 	public boolean isNodeResolvingTo(ASTNode node, String signature) {
+		if (signature == null || signature.trim().length() == 0) {
+			return true;
+		}
+		Binding b = resolveNodeToBinding(node);
+		if (b instanceof ParameterizedTypeBinding) {
+			ParameterizedTypeBinding ptb = (ParameterizedTypeBinding) b;
+			return resolvesReferenceBindingTo(ptb.actualType(), signature);
+		} else if (b instanceof BaseTypeBinding) {
+			return (new String(b.readableName()).equals(signature));
+		} else if (b instanceof TypeBinding) {
+			return resolvesReferenceBindingTo((TypeBinding) b, signature);
+		}
+		
 		return true;
 	}
 	
-	public boolean isNodeComplexType(ASTNode node, String signature) {
+	private boolean resolvesReferenceBindingTo(TypeBinding sb, String signature) {
+		if (sb == null) {
+			return false;
+		}
+		
+		if (new String(sb.readableName()).startsWith(signature) || (sb instanceof ArrayBinding && "array".equals(signature))) {
+			return true;
+		}
+		List<ReferenceBinding> bindings = new ArrayList<>();
+		Collections.addAll(bindings, sb.superInterfaces());
+		bindings.add(sb.superclass());
+		boolean result = false;
+		Iterator<ReferenceBinding> it = bindings.iterator();
+		while (it.hasNext() && result == false) {
+			result = resolvesReferenceBindingTo(it.next(), signature);
+		}
+		return result;
+	}
+	
+	public boolean resolvesReferenceBindingToArray(TypeBinding sb) {
+		return sb instanceof ArrayBinding;
+	}
+	
+	public boolean isNodePrimitiveType(ASTNode node) {
 		return true;
 	}
 	
 	public boolean isNodeBooleanExpression(ASTNode node) {
 		return true;
 	}
-
-	private String resolveNodeToTypeString(ASTNode node) {
-		
+	
+	private Binding resolveNodeToBinding(ASTNode node) {
 		if (node instanceof NameReference) {
     		NameReference nr = (NameReference) node;
 			if (nr.binding instanceof VariableBinding) {
 				VariableBinding vb = (VariableBinding) nr.binding;
-				return new String(vb.type.readableName());
+				return vb.type;
 			}
 		} else if (node instanceof FieldReference) {
     		FieldReference fr = (FieldReference) node;
-    		
-			return new String(fr.receiver.resolvedType.readableName());
+			return fr.receiver.resolvedType;
 		}
-		// TODO
+		return null;
+	}
+
+	private String resolveNodeToTypeString(ASTNode node) {
+		Binding b = resolveNodeToBinding(node);
+		if (b != null) {
+			return new String(b.readableName());
+		}
 		return "java.lang.Object";
 	}
 	
