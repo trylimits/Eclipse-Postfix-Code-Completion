df69577c9892d79fe0f207e4ee19c29984bf70b9
Nicolaj Hoess <nicohoess@gmail.com>
2014-06-15 09:33:38 +0200

    Extended NewFieldResolver to support creation of constants and initialized fields.

--- org.eclipse.jdt.postfixcompletion/src/org/eclipse/jdt/postfixcompletion/resolver/NewFieldResolver.java
+++ org.eclipse.jdt.postfixcompletion/src/org/eclipse/jdt/postfixcompletion/resolver/NewFieldResolver.java
@@ -14,7 +14,15 @@ import org.eclipse.text.edits.TextEdit;
 
 @SuppressWarnings("restriction")
 public class NewFieldResolver extends TemplateVariableResolver {
-
+	
+	private static final int PUBLIC_ARG = 1;
+	private static final boolean PUBLIC_DEFAULT_VALUE = false;
+	private static final int FORCE_STATIC_ARG = 2;
+	private static final boolean FORCE_STATIC_DEFAULT_VALUE = false;
+	private static final int FINAL_FIELD_ARG = 3;
+	private static final boolean FINAL_FIELD_DEFAULT_VALUE = false;
+	private static final int INIT_VALUE_ARG = 4;
+	private static final boolean INIT_VALUE_DEFAULT_VALUE = false;
 
 	private final String defaultType;
 
@@ -54,14 +62,18 @@ public class NewFieldResolver extends TemplateVariableResolver {
 
 			type = refVar.getParamType();
 		}
+		
+		boolean initValue = getInitValueParam(params);
+		boolean forceStatic = getForceStaticParam(params);
+		boolean publicField = getPublicParam(params);
+		boolean finalField = getFinalField(params);
 
-//		String newType = jc.addImport(type);
 		String newType = type;
-		String[] names = jc.suggestFieldName(newType);
+		String[] names = jc.suggestFieldName(newType, finalField, forceStatic);
 		
 		mv.setChoices(names);
 		
-		TextEdit te = jc.addField(newType, names[0]);
+		TextEdit te = jc.addField(newType, names[0], publicField, forceStatic, finalField, (initValue && ref instanceof JavaVariable) ? getValueFromVariable((JavaVariable) ref) : null);
 		if (te != null) {
 			jc.markAsUsed(names[0]);
 			// We can apply it to the context
@@ -74,9 +86,61 @@ public class NewFieldResolver extends TemplateVariableResolver {
 		}
 	}
 	
+	private boolean getPublicParam(List<String> params) {
+		if (params.size() >= PUBLIC_ARG + 1) {
+			String val = params.get(PUBLIC_ARG);
+			return convertBooleanStringParam(val);
+		}
+		return PUBLIC_DEFAULT_VALUE;
+	}
+	
+	private boolean getForceStaticParam(List<String> params) {
+		if (params.size() >= FORCE_STATIC_ARG + 1) {
+			String val = params.get(FORCE_STATIC_ARG);
+			return convertBooleanStringParam(val);
+		}
+		return FORCE_STATIC_DEFAULT_VALUE;
+	}
+	
+	private boolean getInitValueParam(List<String> params) {
+		if (params.size() >= INIT_VALUE_ARG + 1) {
+			String val = params.get(INIT_VALUE_ARG);
+			return convertBooleanStringParam(val);
+		}
+		return INIT_VALUE_DEFAULT_VALUE;
+	}
+	
+	private boolean getFinalField(List<String> params) {
+		if (params.size() >= FINAL_FIELD_ARG + 1) {
+			String val = params.get(FINAL_FIELD_ARG);
+			return convertBooleanStringParam(val);
+		}
+		return FINAL_FIELD_DEFAULT_VALUE;
+	}
+	
+	private String getValueFromVariable(JavaVariable var) {
+		for (String s : var.getValues()) {
+			if (s != null && s.trim().length() > 0) {
+				return s;
+			}
+		}
+		return null;
+	}
+	
+	private boolean convertBooleanStringParam(String value) {
+		if ("true".equals(value)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
 	private int findAbsolutePositionOfFieldNameBeginning(IDocument doc, TextEdit te, String name) {
 		try {
 			String temp = doc.get(te.getOffset(), te.getLength());
+			if (temp.contains("=")) {
+				temp = temp.substring(0, temp.indexOf("="));
+			}
 			int nameOcc = temp.lastIndexOf(name);
 			if (nameOcc != -1) {
 				return te.getOffset() + nameOcc;
